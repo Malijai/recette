75e84d3bc3e51f98237d3f3c1e2d0a24868aaeb5
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-11-25 12:31:35 -0500

    Ajout d'un templatetag maison pour formater les nombres et quelques modifications cosmétiques aux ingrédients

---
+++ templatetags/formatenombre.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+from django import template
+from django.apps import apps
+
+register = template.Library()
+
+
+@register.filter
+def int_format(value):
+    value = str(value)
+    entier, decimale = value .split('.')
+    if decimale == '00':
+        return entier
+    else:
+        return value
