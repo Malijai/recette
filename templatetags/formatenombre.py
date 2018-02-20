4afb896d782a53734f996a868be3c54540ff78cf
malijai <malijai.caulet@videotron.ca>
2018-02-20 17:47:47 -0500

    Ajout de default if none et correction des .00 suivant que c'Est un . ou une ,

--- templatetags/formatenombre.py
+++ templatetags/formatenombre.py
@@ -10,10 +10,14 @@ register = template.Library()
 def int_format(value):
     value = str(value)
     m = re.search('\.',value)
-    if m:
+    n = re.search(',',value)
+    if m or n:
         entier, decimale = value .split('.')
+        entier2, decimale2 = value .split(',')
         if decimale == '00':
             return entier
+        elif decimale2 == '00':
+            return entier2
         else:
             return value
     else:
